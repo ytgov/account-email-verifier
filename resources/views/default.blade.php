cd17e967045c01d2ac46bd913e939b98c2c883d7
Lee O'Mara <lee.omara@gov.yk.ca>
2022-01-12 15:11:11 -0700

    Content for default view

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,9 +1,37 @@
 <html>
+    <head> 
+        <title>Verify your email address - Government of Yukon online services account</title>
+    </head> 
     <body>
-        <h1>Hello</h1>
+        <h1>Yukon</h1>
+
+        <h1>Verify your email address</h1>
+
+        <p>Your email address needs to be verified before you can continue.</p>
+
+
+        <h2>Check your email</h2>
         
+        <p>We have sent an email message to {{ $email }}.</p>
+        
+        <p>Choose the Verify your email address button in that message. </p>
+          
+        <p>After you verify, you can close this browser window.</p>
+
+
+        <h2>Didn't get an email?</h2>
+
+        <p>It can sometimes take a few minutes for a message to arrive. Check your Spam or Junk folder if you can't find it. </p>
+        
+        <p>If you lost your verification email or want to request a new email, we can send another message.</p>
+
+        <button>Send a new verification email</button>
+        
+
+        <h2>Get help with your account</h2>
+
         <p>
-          We have sent a message to {{ $email }}
+          Use the <a href="#">account support service</a> to get help. We aim to respond to questions in 5 working days.
         </p>
     </body>
 </html>
