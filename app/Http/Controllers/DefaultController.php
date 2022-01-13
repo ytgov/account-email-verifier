c6ec321cbe86f874883258b5ba281849ee23063f
Lee O'Mara <lee.omara@gov.yk.ca>
2022-01-13 16:48:59 -0700

    Change parseSessionToken
    make the parsing, verification and validation more explicit

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -38,26 +38,15 @@ class DefaultController extends Controller
           Log::error('Session token is missing');
           abort(400, "Required information is missing.");
         }
-        if (!$this->sessionTokenIsValid($rawSessionToken)) {
-          // This is a configuration error or a security issue.
-          // TODO Improve the response to the user here.
-          Log::error('Session token is invalid');
-          abort(400, "Unable to load user information.");
-        }
-        if (!$this->sessionTokenNotExpired($rawSessionToken)) {
-          // User should go back to Auth0
-          Log::error('Session token has expired');
-          // TODO show the user an error message. Have them access
-          // Auth0 again?
-          abort(400, "Session has expired");
-        }
 
-        $sessionToken = $this->parseSessionToken($rawSessionToken);
+        $token = $this->parseSessionToken($rawSessionToken);
+
+        $sessionToken = $token->toArray();
 
         $continueUrl = $this->continueLink($state);
 
         return view('default', [
-          'email' => $sessionToken->email,
+          'email' => $sessionToken['email'],
           'continueUrl' => $continueUrl,
           'sessionToken' => $rawSessionToken,
           'state' => $state,
@@ -79,10 +68,11 @@ class DefaultController extends Controller
         $rawSessionToken = $request->input('session_token');
 
         // Validate token here.
-        $sessionToken = $this->parseSessionToken($rawSessionToken);
+        $token = $this->parseSessionToken($rawSessionToken);
+        $sessionToken = $token->toArray();
         // gold plating: check if the user is already verified.
         // Have Auth0 re-send the verification message.
-        $this->auth0ResendMessage($sessionToken->user_id, $sessionToken->application_id);
+        $this->auth0ResendMessage($sessionToken['user_id'], $sessionToken['application_id']);
 
         // Redirect to the default page to show a confirmation message.
         return redirect()->route('default', [
@@ -99,51 +89,33 @@ class DefaultController extends Controller
     }
 
     /**
-     * Is the Session Token (JWT) valid?
-     *
-     * @param string $sessionToken The raw JWT
-     * @return return bool
-     */
-    private function sessionTokenIsValid($sessionToken)
-    {
-      // FIXME Replace this placeholder.
-      return true;
-    }
-
-    /**
-     * Is the Session Token (JWT) still fresh?
-     *
-     * @param string $sessionToken The raw JWT
-     * @return return bool
-     */
-    private function sessionTokenNotExpired($sessionToken)
-    {
-      // FIXME Replace this placeholder.
-      return true;
-    }
-
-    /**
      * Return session information from the JWT.
      *
      * @param string $sessionToken The raw JWT
-     * @return return obj
+     * @return return \Auth0\SDK\Token
      */
     private function parseSessionToken($rawJWT)
     {
-      // From https://auth0.com/docs/quickstart/backend/php/
-      // Trim whitespace from token string.
-      $jwt = trim($rawJWT);
+      $auth0 = $this->getSdk();
+      $token = new \Auth0\SDK\Token($auth0->configuration(), $rawJWT, \Auth0\SDK\Token::TYPE_ID_TOKEN);
 
-      // Attempt to decode the token:
       try {
-          $token = $this->getSdk()->decode($jwt, null, null, null, null, null, null, \Auth0\SDK\Token::TYPE_TOKEN);
-          return (object) $token->toArray();
+        // Verify the token: (This will throw an \Auth0\SDK\Exception\InvalidTokenException if verification fails.)
+        $token->verify('HS256', NULL, env('AUTH0_SESSION_TOKEN_SECRET'));
+
+        // Validate the token claims: (This will throw an \Auth0\SDK\Exception\InvalidTokenException if validation fails.)
+        $token->validate();
       } catch (\Auth0\SDK\Exception\InvalidTokenException $exception) {
-          // The token wasn't valid. Let's display the error message from the Auth0 SDK.
-          // We'd probably want to show a custom error here for a real world application.
-          // TODO Fail better here.
-          abort(400, $exception->getMessage());
+        // The token wasn't valid. Let's display the error message from the Auth0 SDK.
+        // We'd probably want to show a custom error here for a real world application.
+        // if token is not valid, this is a configuration error or a security issue.
+        // if token has  expired, User should go back to Auth0?
+        Log::error('Token invalid', ['token' => $token, 'exception' => $exception->getMessage()]);
+        // TODO Fail better here.
+        abort(400, $exception->getMessage());
       }
+      
+      return $token;
     }
     
     private function getSdk()
