81e894c599edb1d8dd3830a52eea83eb9fecac35
Lee O'Mara <lee.omara@gov.yk.ca>
2022-01-13 12:44:12 -0700

    Make the support link configurable though .env

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -98,7 +98,7 @@
         <h4>Get help with your account</h4>
 
         <p>
-          Use the <a href="#">account support service</a> to get help. We aim to respond to questions in 5 working days.
+          Use the <a href="{{ env('SUPPORT_URL') }}">account support service</a> to get help. We aim to respond to questions in 5 working days.
         </p>
 
 
