bd996cbf52bf54d0f7a83877ea2147cbd75c0309
Lee O'Mara <lee.omara@gov.yk.ca>
2022-01-12 20:19:46 -0700

    stub out some of the logic for loading the default page

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -26,22 +26,38 @@ class DefaultController extends Controller
     public function show(Request $request)
     {
         $state = $request->input('state');
-        $session_token = $request->input('session_token');
+        $rawSessionToken = $request->input('session_token');
         if (empty($state)) {
           // TODO Improve the response to the user here.
           Log::error('State is missing');
-          abort(500, "Required information is missing.");
+          abort(400, "Required information is missing.");
         }
 
-        if (empty($session_token)) {
+        if (empty($rawSessionToken)) {
           // TODO Improve the response to the user here.
-          Log::error('Session token is missing');          
-          abort(500, "Required information is missing.");
+          Log::error('Session token is missing');
+          abort(400, "Required information is missing.");
         }
+        if (!$this->sessionTokenIsValid($rawSessionToken)) {
+          // This is a configuration error or a security issue.
+          // TODO Improve the response to the user here.
+          Log::error('Session token is invalid');
+          abort(400, "Unable to load user information.");
+        }
+        if (!$this->sessionTokenNotExpired($rawSessionToken)) {
+          // User should go back to Auth0
+          Log::error('Session token has expired');
+          // TODO show the user an error message. Have them access
+          // Auth0 again?
+          abort(400, "Session has expired");
+        }
+
+        $sessionToken = $this->parseSessionToken($rawSessionToken);
 
         $continueUrl = $this->continueLink($state);
+
         return view('default', [
-          'email' => 'tester@gmail.com',
+          'email' => $sessionToken->emailAddress,
           'continueUrl' => $continueUrl
         ]);
     }
@@ -50,5 +66,45 @@ class DefaultController extends Controller
     {
       $idp_domain = env('IDP_DOMAIN', 'auth0.com');
       return 'https://' . $idp_domain . '/continue?state=' . $state; 
+    }
+
+    /**
+     * Is the Session Token (JWT) valid?
+     *
+     * @param string $sessionToken The raw JWT
+     * @return return bool
+     */
+    private function sessionTokenIsValid($sessionToken)
+    {
+      // FIXME Replace this placeholder.
+      return true;
+    }
+
+    /**
+     * Is the Session Token (JWT) still fresh?
+     *
+     * @param string $sessionToken The raw JWT
+     * @return return bool
+     */
+    private function sessionTokenNotExpired($sessionToken)
+    {
+      // FIXME Replace this placeholder.
+      return true;
+    }
+
+    /**
+     * Return session information from the JWT.
+     *
+     * @param string $sessionToken The raw JWT
+     * @return return obj
+     */
+    private function parseSessionToken($sessionToken)
+    {
+      // FIXME Replace this placeholder.
+      return (object) [
+        'applicationID' => '123456789',
+        'userID' => 'abcefghijklmnop',
+        'emailAddress' => 'franky@hollywood.com'
+      ];
     }
 }
