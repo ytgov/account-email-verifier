cb874eaf9432d5989cc412071a8132ce7fc4ff78
Lee O'Mara <lee.omara@gov.yk.ca>
2022-01-12 17:35:23 -0700

    Mostly styled in line with Yukon.ca design system

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,11 +1,60 @@
-<html>
-    <head> 
-        <title>Verify your email address - Government of Yukon online services account</title>
-    </head> 
-    <body>
-        <h1>Yukon</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-        <h1>Verify your email address</h1>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+  <meta name="theme-color" content="#000000">
+  <title>Verify your email address - Government of Yukon online services account</title>
+  <!-- <link rel="stylesheet" href="/libraries/yukon-ca-design-system/yukon-design-system-template/static/css/main.css"> -->
+  <link rel="stylesheet" href="/libraries/yukon-ca-design-system/yukon-design-system-template/static/css/main.max.css">
+  <link type="image/x-icon" rel="icon" href="/libraries/yukon-ca-design-system/yukon-design-system-template/favicon.ico">
+</head>
+
+<body class="transactional-services" id="top">
+<header id="navbar" role="banner">
+  <div>
+    <div id="wb-bnr">
+      <div class="container">
+        <div class="row">
+          <div class="site-main-logo col-6">
+            <div>
+              <a href="/"><object id="header-logo" data="/libraries/yukon-ca-design-system/yukon-design-system-template/images/logo.svg" role="img" type="image/svg+xml" alt="Government of Yukon Logo" tabindex="-1">
+                <img alt="Government of Yukon Logo" src="/libraries/yukon-ca-design-system/yukon-design-system-template/images/logo.png">
+              </object></a>
+            </div>
+          </div>
+          <div class="site-nav-toggle col-6">
+            <div class="row">
+              <div class="pull-right">
+                <!-- 
+                <section id="wb-lng" class="header-lang-toggle">
+                  <ul class="text-right">
+                    <li id="wet-fullhd-lang"><a href="/fr" class="link" hreflang="fr" lang="fr">Français</a></li> 
+                  </ul>
+                </section>
+              -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<div class="aurora-town">
+  <span class="aurora--main"></span>
+</div>
+<div id="content">
+  <div class="content container">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="container" style="margin-bottom: 50px;">
+            <div class="row">
+              <div style="min-height: 600px; padding-top: 30px;" class="col">
+
+                <h1 class="mrgn-bttm-lg">Verify your email address</h1>
 
         <p>Your email address needs to be verified before you can continue.</p>
 
@@ -25,7 +74,7 @@
         
         <p>If you lost your verification email or want to request a new email, we can send another message.</p>
 
-        <button>Send a new verification email</button>
+        <button class="btn btn-primary">Send a new verification email</button>
         
 
         <h2>Get help with your account</h2>
@@ -33,5 +82,56 @@
         <p>
           Use the <a href="#">account support service</a> to get help. We aim to respond to questions in 5 working days.
         </p>
-    </body>
-</html>
+
+
+        
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+
+        <footer id="wb-info" role="contentinfo">
+
+        <div class="brand">
+          <div class="container">
+            <div class="row">
+              <div class="brand-items col-12">
+                <div class="site-main-logo mrgn-lft-lg">
+                  <object id="footer-logo" data="/libraries/yukon-ca-design-system/yukon-design-system-template/images/logo-white.svg" role="img" type="image/svg+xml" alt="Government of Yukon Logo" tabindex="-1">
+                    <img alt="Government of Yukon Logo" src="/libraries/yukon-ca-design-system/yukon-design-system-template/images/logo-white.png">
+                  </object>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="footer-menu-wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="d-flex flex-row col-6">
+                <ul class="footer-menu navbar-left nav">
+                  <section id="block-menu-menu-wet-footer" class="block block-menu clearfix">
+                    <ul class="nav navbar-nav">
+                      <li><a href="https://yukon.ca" title="Go to Yukon.ca">Home</a></li>
+                    </ul>
+                  </section>
+                </ul>
+              </div>
+              <div class="d-flex flex-row-reverse col-6">
+                <div class="site-desc-wrap navbar-right">
+                  <p class="site-desc">
+                    © Copyright 2022 Government of Yukon
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </footer>
+        </body>
+        </html>
