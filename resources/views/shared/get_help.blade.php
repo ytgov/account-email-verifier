8fe1541240ad7b418a47f5ceb0052e8c903c835c
Lee O'Mara <lee.omara@gov.yk.ca>
2022-02-21 15:21:14 -0700

    "We aim to respond to questions in 1 working day"

--- resources/views/shared/get_help.blade.php
+++ resources/views/shared/get_help.blade.php
@@ -2,6 +2,6 @@
   <h4>Get help with your account</h4>
 
   <p>
-    Use the <a href="{{ env('SUPPORT_URL') }}">account support service</a> to get help. We aim to respond to questions in 5 working days.
+    Use the <a href="{{ env('SUPPORT_URL') }}">account support service</a> to get help. We aim to respond to questions in 1 working day.
   </p>
 
