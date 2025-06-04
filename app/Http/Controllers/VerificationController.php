88014f7f8f9bb354b8b163b8725cc90a60103699
Lee O'Mara <lee.omara@gov.yk.ca>
2025-06-03 17:59:35 -0700

    work in progress from 2024-04

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -50,7 +50,7 @@ class VerificationController extends BaseController
             "timestamp" => date('c'),
             "status" => 500,
             "error" => "Internal Server Error",
-            "message" => "Email send failed",
+            "message" => "Receipt email send failed",
             "detail" => $ex->getMessage(),
             "path" => "/verification/submitted"]
          , 500);
@@ -61,5 +61,67 @@ class VerificationController extends BaseController
             "status" => 200,
             "message" => "Receipt processed"], 200);
 
+    }
+
+    /**
+     * Handle verification submissions that have been reviewed.
+     *
+     * Some reviews are prositive, others are negative. Positive reviews
+     * trigger an email to the user. Negative reviews trigger an email
+     * to eServices service desk to follow up with the user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function completed(Request $request)
+    {
+      // TODO protect this endpoint.
+      // TODO add logging.
+      foreach(['requestStatus', 'requestResult', 'userClaims'] as $input ) {
+          if(empty($request->input($input))) {
+              Log::info('Required info missing');
+              return response()->json([
+                "timestamp" => date('c'),
+                "status" => 400,
+                "error" => "Bad request",
+                "message" => "Required value missing: $input",
+                "path" => "/verification/completed"]
+             , 400);
+          }
+      }
+
+      $requestResult = $request->input('requestResult');
+
+      if($requestResult != 'APPROVED') {
+        Log::warning("Result is not approved, bail here.");
+        return response([
+            "timestamp" => date('c'),
+            "status" => 200,
+            "message" => "Submission received, but not approved"], 200);
+      }
+
+      // Send the "approved" message to the user.
+      $recipient = $this->getApprovedRecipient($this->request->input('userClaims'));
+
+      try {
+          Mail::to($recipient)
+            ->send(new VerificationApproved());
+          // Do we know if the send worked?
+      } catch (\Exception $ex) {
+          Log::error('Email send failed');
+          return response()->json([
+            "timestamp" => date('c'),
+            "status" => 500,
+            "error" => "Internal Server Error",
+            "message" => "Approved email send failed",
+            "detail" => $ex->getMessage(),
+            "path" => "/verification/completed"]
+         , 500);
+      }
+
+      return response([
+            "timestamp" => date('c'),
+            "status" => 200,
+            "message" => "Receipt processed"], 200);
     }
 }
